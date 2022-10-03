fa914ad163bebaa411d3adfd4308ab90fa945c65
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-03 03:40:07 +0000

    0.1.1 Updated `name` module
    - Removed obsolete `s3d-post-flow`
    - Updated source for `github.com` and new `name` module

--- CHANGES.md
+++ CHANGES.md
@@ -1,8 +1,9 @@
 # Changes
 Recently completed and planned work is tracked here.
 
-## [0.0.0](.) [0.0.5](.)
-- Created initial resources
+## [0.0.0](.) [0.1.0](.)
+- Created early versions
 
-## [0.1.0](.)
-- Created 1st release
+## [0.1.1](.)
+- Removed obsolete `s3d-post-flow`
+- Updated source for `github.com` and new `name` module
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "name" {
-  source = "git::https://github.com/s3d-club/terraform-external-data-name-tags?ref=v0.1.0"
+  source = "github.com/s3d-club/terraform-external-name?ref=v0.1.1"
 
-  name_prefix = var.name_prefix
-  path        = path.module
-  tags        = var.tags
+  context = var.name_prefix
+  path    = path.module
+  tags    = var.tags
 }
 
 resource "aws_ecr_repository" "this" {
-  name         = module.name.name_prefix
+  name         = module.name.prefix
   tags         = module.name.tags
   force_delete = true
 
